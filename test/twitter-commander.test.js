06f3b7d8c96bd04c534109587958396e956f0d4c
Mike Groseclose <mike.groseclose@gmail.com>
2014-09-29 08:40:03 -0700

    refactor

---
+++ test/twitter-commander.test.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var expect = require('chai').expect;
+var sinon = require('sinon');
+
+//var twitterCommander = require('../lib/twitter-commander.js');
+
+describe('twitterCommander', function () {
+  describe('run', function() {
+    describe('when no creds are given', function() {
+
+    });
+
+    describe('when creds are given', function() {
+
+    });
+  });
+});
